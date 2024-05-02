3cdfc4fff049516a3f8260d0576417d81cc17187
xBl0oM <111044215+xBl0oM@users.noreply.github.com>
2024-05-02 20:41:21 +0200

    changed program and startscreen, added roulette

--- Startscreen.cs
+++ Startscreen.cs
@@ -16,46 +16,54 @@ namespace Roulette
         private Bank _bank;
         public bool Blackjack;
         public bool Roulette;
+        private int _selectedCredits;
 
         public StartScreen(User user, Bank bank)
         {
             _user = user;
             _bank = bank;
-
         }
 
         public void ConvertToCredits()
         {
             Console.WriteLine("Möchten Sie ihre wertvollen Batzen in noch wertvollere Credits umwandeln? [ja/nein] ");
-            string userResponse = "";
-            bool tryCatch = false;
+            string userResponse = Console.ReadLine();
 
-            while (!tryCatch)
-            {
-                try
-                {
-                    userResponse = Console.ReadLine();
-                    tryCatch = true;
-                }
-                catch
-                {
-                    Console.WriteLine("Na Hoppla, das habe ich nicht verstanden.");
-                }
-            }
             if (userResponse == "ja")
             {
                 Console.WriteLine("Geben Sie den Betrag ein, den Sie in Credits umwandeln möchten:");
-                int amount = Convert.ToInt32(Console.ReadLine());
-                _user.UserCredits += amount;
-                Console.WriteLine($"Sie haben erfolgreich {amount} Credits erhalten.");
+                int? amount = null;
+                bool isNumberValid = false;
+                while (!isNumberValid)
+                {
+                    try
+                    {
+                        amount = Convert.ToInt32(Console.ReadLine());
+                        isNumberValid = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Na Hoppla, das habe ich nicht verstanden.");
+                    }
+                }
+
+                if (amount.HasValue)
+                {
+                    _selectedCredits = amount.Value;
+                    _user.UserCredits += _selectedCredits;
+                    Console.WriteLine($"Sie haben erfolgreich {_selectedCredits} Credits erhalten.");
+                }
+                else
+                {
+                    Console.WriteLine("Keine gültige Eingabe. Credits wurden nicht hinzugefügt.");
+                }
             }
             else
             {
-                Console.WriteLine("Schade, in dem Fall werden Sie heute kein Gewinner des Lebens und im Casino werden.");
+                Console.WriteLine("Schade, in dem Fall werden Sie heute kein Gewinner des Lebens und im Casino.");
             }
-
-
         }
+
         public void SelectGame()
         {
             Console.WriteLine("Auf was haben Sie mehr Lust? Blackjack oder Roulette");
@@ -74,23 +82,16 @@ namespace Roulette
                 }
             }
 
-            switch (selectResponse)
+            switch (selectResponse.ToLower()) // Kleinschreibung für den Vergleich
             {
                 case "roulette":
                     Console.WriteLine("Sie haben Roulette gewählt.");
-                    Roulette = true;
-                    break;
-                case "Roulette":
-                    Console.WriteLine("Sie haben Roulette gewählt.");
-                    Roulette = true;
+                    RouletteGame rouletteGame = new RouletteGame(_user, _bank);
+                    rouletteGame.Play();
                     break;
                 case "blackjack":
                     Console.WriteLine("Sie haben Blackjack gewählt.");
-                    Blackjack = true;
-                    break;
-                case "Blackjack":
-                    Console.WriteLine("Sie haben Blackjack gewählt.");
-                    Blackjack = true;
+                    // Hier können Sie den Aufruf für das Blackjack-Spiel einfügen, wenn Sie es implementieren.
                     break;
                 default:
                     Console.WriteLine("Ungültige Auswahl.");
@@ -98,4 +99,5 @@ namespace Roulette
             }
         }
     }
+
 }
